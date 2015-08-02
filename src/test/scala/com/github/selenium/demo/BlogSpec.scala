3bcb130fb8d3303ebd010f35568669f7ac2ef41b
Mark Perry <maperry78@yahoo.com.au>
2015-08-02 12:07:40 +1000

    Run scalatest and generate reports using gradle

--- src/test/scala/com/github/selenium/demo/BlogSpec.scala
+++ src/test/scala/com/github/selenium/demo/BlogSpec.scala
@@ -1,18 +1,21 @@
 package com.github.selenium.demo
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, ShouldMatchers}
 import org.scalatest.selenium.HtmlUnit
 
 /**
  * Created by MarkPerry on 2/08/2015.
  */
+@RunWith(classOf[JUnitRunner])
 class BlogSpec extends FlatSpec with ShouldMatchers with HtmlUnit {
 
 //	implicit val webDriver: WebDriver = new HtmlUnitDriver
 
 	val host = "http://www.google.com/"
 
-	"The blog app home page" should "have the correct title" in {
+	"The Google home page" should "have the correct title" in {
 		go to (host)
 		pageTitle should be ("Google")
 	}
